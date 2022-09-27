2e04eab0a6a0272756503614b8aa0d963d113c4f
code1218 <skjil1218@gmail.com>
2022-09-27 20:59:38 +0900

    init

---
+++ src/main/java/com/study/mvc20220927junil/Mvc20220927JunilApplication.java
@@ -0,0 +1,13 @@
+package com.study.mvc20220927junil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc20220927JunilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc20220927JunilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/mvc20220927junil/Mvc20220927JunilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mvc20220927junil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc20220927JunilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
